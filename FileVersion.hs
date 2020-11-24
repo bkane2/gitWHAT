e364a1caef2db9ff51dd0c69c3efc513971c05f4
bkane2 <Ben.kane@me.com>
2020-11-24 17:44:49 -0500

    Added accessor functions

--- FileVersion.hs
+++ FileVersion.hs
@@ -2,15 +2,17 @@ module FileVersion where
 
 import Model as M
 
--- Create a new file version from a bytestring
--- TODO: if we're just maintaining a database of IDs somewhere and not using a SHA1 hash,
--- we can remove the 'hash' function here and probably get rid of the argument requirement
--- of the two parent nodes entirely.
+-- Create a new file version from a bytestring and two parents
 createVersion :: FileContents -> (NodeID, NodeID) -> FileVersion
--- Inputs:
---  contents :: FileContents : a byte string of the file contents
---  (p1, p2) :: (NodeID, NodeID) : a tuple of two parent NodeIDs
--- Returns:
---  version :: FileVersion : a version with a NodeID created from hashing contents and parent NodeIDs
 createVersion contents (p1, p2) =
   ((hashNodeID (contents <> (intToByteString p1) <> (intToByteString p2))), contents)
+
+-- Retrieve contents of FileVersion
+getVersionContents :: FileVersion -> FileContents
+getVersionContents version = contents
+  where (id, contents) = version
+
+-- Retrieve NodeID of FileVersion
+getVersionNodeID :: FileVersion -> NodeID
+getVersionNodeID version = id
+  where (id, contents) = version
