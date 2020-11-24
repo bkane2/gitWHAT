0f7d73053934d58d0fc3b77fb8d037c8c2ee0183
bkane2 <Ben.kane@me.com>
2020-11-24 17:12:08 -0500

    Fixed createFileLog function def

--- FileLog.hs
+++ FileLog.hs
@@ -1,10 +1,11 @@
 module FileLog where
 
+import Tree as G
 import Model as M
 
 -- Create an empty FileLog
-createFileLog :: () -> FileLog
-createFileLog () = Empty
+createFileLog :: FileLog
+createFileLog = Empty
 
 -- add_version :: FileLog -> FileVersion -> FileLog
 -- get_version :: FileLog -> Int -> FileVersion
