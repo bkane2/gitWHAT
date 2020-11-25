cb8ba7267d1d623215adde8f90e55d42ff23368e
bkane2 <Ben.kane@me.com>
2020-11-25 01:38:12 -0500

    Added functions for adding and getting versions from a log

--- FileLog.hs
+++ FileLog.hs
@@ -1,11 +1,38 @@
 module FileLog where
 
-import Tree as G
+import Tree as T
 import Model as M
+import FileVersion as FV
 
 -- Create an empty FileLog
 createFileLog :: FileName -> FileLog
-createFileLog fname = (fname, Empty)
+createFileLog fname = (fname, T.createNode (FV.createEmptyVersion))
 
--- add_version :: FileLog -> FileVersion -> FileLog
--- get_version :: FileLog -> Int -> FileVersion
+-- Retrieve a version from a FileLog by NodeID
+-- Note: version is assumed to exist; if not this creates and
+--       returns a new empty FileVersion.
+getVersion :: FileLog -> NodeID -> FileVersion
+getVersion log id =
+  let (_, tree) = log
+  in case (T.getNodeKey id FV.getVersionNodeID tree) of
+    Just a -> a
+    Nothing -> (FV.createEmptyVersion)
+
+-- Adds a version 
+addVersion :: FileLog -> FileVersion -> NodeID -> Maybe NodeID -> FileLog
+addVersion log version p1 p2Maybe =
+  let (fname, tree) = log
+      tree2Maybe = case p2Maybe of
+        Just p2 -> T.addTwoParentsKey version p1 p2 FV.getVersionNodeID tree
+        Nothing -> T.addOneParentKey version p1 FV.getVersionNodeID tree
+      tree2 = case tree2Maybe of
+        Just a -> a
+        Nothing -> T.createNode version
+  in (fname, tree2)
+
+-- Looks up a FileLog in a list of FileLogs, or creates a new FileLog if it doesn't exist
+fileLogLookup :: FileName -> [FileLog] -> FileLog
+fileLogLookup fname [] = createFileLog fname
+fileLogLookup fname (x:xs) =
+  let (fname1, _) = x
+  in if fname1 == fname then x else (fileLogLookup fname xs)
