a36bfdc088a9297d95cf99b7c5d22b29011ec46a
bkane2 <Ben.kane@me.com>
2020-11-25 01:39:01 -0500

    Added assorted function tests (to be cleaned later)

--- TestModel.hs
+++ TestModel.hs
@@ -6,9 +6,23 @@ import Repository as RP
 import Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C
 
-main = do
-  content <- BS.readFile "test/a.txt"
-  print (FV.createVersion content (-1154686932835703469, 0))
+import Data.Map (Map)
+import qualified Data.Map as Map
 
-  -- FL.createFileLog
-  -- print (show filelog)
+main = do
+  -- TODO: clean up
+  content <- BS.readFile "test/a.txt"
+  let file1 = ("test/a.txt", content)
+  let repo = RP.initRepository "testRepo"
+  print (FV.createVersion content (-1154686932835703469, 0))
+  print repo
+  let (_, (lastRev:_), _, _) = repo
+  print lastRev
+  -- let repo2 = RV.revise repo "test revision" lastRev ["test/a.txt"]
+  let repo2 = RV.revise repo "test revision" lastRev [file1]
+  print repo2
+  -- print (nodeIDListToContents [("fname1", 0), ("fname2", 1)])
+  -- 
+  contentMan <- BS.readFile "test/manifest_test.txt"
+  let manMap = RV.nodeIDMapFromManifest contentMan ["fname1", "fname2"]
+  print (Map.keys manMap)
