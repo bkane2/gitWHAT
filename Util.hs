bd51c326cb3bf8bc5b1293b20fcaa388361bc593
bkane2 <Ben.kane@me.com>
2020-11-27 17:17:01 -0500

    Added a couple more utility functions

--- Util.hs
+++ Util.hs
@@ -3,6 +3,7 @@ module Util where
 import Data.Hashable as H
 import Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C
+import Data.List as L
 
 -- Create NodeID by hashing on ByteString
 hashNodeID :: ByteString -> Int
@@ -21,9 +22,18 @@ emptyByteString :: ByteString
 emptyByteString = BS.empty
 
 -- Split string by comma
-splitComma :: String -> [String]
-splitComma [] = []
-splitComma (c:cs)
-  | c == ','  = "" : rest
+splitString :: String -> Char -> [String]
+splitString [] _ = []
+splitString (c:cs) chr
+  | c == chr = "" : rest
   | otherwise = (c : Prelude.head rest) : Prelude.tail rest
-  where rest = splitComma cs
+  where rest = splitString cs chr
+
+-- Join a list of strings by some string
+joinString :: String -> [String] -> String
+joinString _ [] = ""
+joinString str lst = (L.intercalate str lst)
+
+-- File IO (wrapper function for loading file)
+loadFile :: String -> IO ByteString
+loadFile fname = BS.readFile fname
