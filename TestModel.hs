2e45a73dc425c05d1429b3adc3c1907b49119306
bkane2 <Ben.kane@me.com>
2020-11-27 16:53:01 -0500

    Cleaned 'revise' test

--- TestModel.hs
+++ TestModel.hs
@@ -1,4 +1,5 @@
 import Model as M
+import Util as U
 import FileVersion as FV
 import FileLog as FL
 import Revision as RV
@@ -10,19 +11,19 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 
 main = do
-  -- TODO: clean up
-  content <- BS.readFile "test/a.txt"
-  let file1 = ("test/a.txt", content)
+  -- Load example files
+  content1 <- BS.readFile "test/a.txt"
+  content2 <- BS.readFile "test/b.txt"
+  let file1 = ("test/a.txt", content1)
+  let file2 = ("test/b.txt", content2)
+
+  -- Init empty repository
   let repo = RP.initRepository "testRepo"
-  print (FV.createVersion content (-1154686932835703469, 0))
-  print repo
+
+  -- Get previous revision
   let (_, (lastRev:_), _, _) = repo
-  print lastRev
-  -- let repo2 = RV.revise repo "test revision" lastRev ["test/a.txt"]
-  let repo2 = RV.revise repo "test revision" lastRev [file1]
+
+  -- Make new revision to repository (after tracking updates in file1 and file2)
+  -- Requires previous revision to be provided as an argument
+  let repo2 = RV.revise repo "test revision" lastRev [file1, file2]
   print repo2
-  -- print (nodeIDListToContents [("fname1", 0), ("fname2", 1)])
-  -- 
-  contentMan <- BS.readFile "test/manifest_test.txt"
-  let manMap = RV.nodeIDMapFromManifest contentMan ["fname1", "fname2"]
-  print (Map.keys manMap)
