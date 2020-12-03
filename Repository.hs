48abb663f02a14a6a4d19dacf5ad2fea23110c10
bkane2 <Ben.kane@me.com>
2020-12-03 15:20:25 -0500

    Removed comment

--- Repository.hs
+++ Repository.hs
@@ -7,23 +7,6 @@ import FileLog as FL
 import Revision as RV
 import Tree as T
 
--- -- Repository consists of a manifest (FileLog) and a list of FileLog for each file being versioned
--- type RepositoryID = String
--- type Repository = (RepositoryID, [Revision], FileLog, [FileLog])
--- -- Revision is a unique RevisionID and a NodeID pointing to a particular version of the manifest FileLog
--- type RevisionID = String
--- type Revision = (RevisionID, NodeID)
--- -- FileLog is a tree of FileVersion (each node is a FileVersion with two parents)
--- type FileID = Int
--- type FileName = String
--- type FileLog = (FileName, Tree FileVersion)
--- -- FileVersion is a unique NodeID together with ByteString contents
--- type NodeID = Int
--- type FileContents = ByteString
--- type FileVersion = (NodeID, FileContents)
--- -- File represents a file, i.e. FileName and FileContents
--- type File = (FileName, FileContents)
-
 -- Create an empty repository
 initRepository :: RepositoryID -> Repository
 initRepository id = (id, [(RV.initRevision)], (FL.createFileLog ".manifest"), [])
