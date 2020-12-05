7208c12288111e11726885a9afadfc4a3f230a93
Melissa Welsh <mwelsh2@u.rochester.edu>
2020-12-04 20:53:47 -0500

    heads command update

--- CommandInterface.hs
+++ CommandInterface.hs
@@ -2,6 +2,7 @@ module CommandInterface where
 
 import System.Environment
 import Text.Read
+import Data.List
 
 import Model as M
 import Util as U
@@ -74,6 +75,10 @@ testForValidityOfCommand command params repoStates
    -- diff <repoName> <revID1> <revID2> [filePath]
    -- TBC
    -- cat <repoName> <revID> <filePath>
+   | (command == "cat") && ((length params) /= 3) = (False, "cat must be called as 'cat <repoName> <revID> <filePath>'")
+   | (command == "cat") && (not (repoExists (params !! 0) repoStates)) = (False, "Repo "++(params !! 0)++" does not exist.")
+   | (command == "cat") && (not (revisionExistsInRepo (params !! 1) (getRepoState (params !! 0) repoStates))) =
+      (False, "Revision "++(params !! 1)++" does not exist in "++(params !! 0)++".")
    -- TBC
    -- checkout <repoName> <revID>
    | (command == "checkout") && ((length params) /= 2) = (False, "checkout must be called as 'checkout <repoName> <revID>'")
@@ -115,7 +120,7 @@ executeCommand "remove" params repoStates = ("Tracking list updated.", (applyToR
 executeCommand "status" params repoStates = ((status (getRepoState (params !! 0) repoStates) (tail params)), repoStates)
 executeCommand "heads" params repoStates = ((heads (getRepoState (params !! 0) repoStates) (tail params)), repoStates)
 executeCommand "diff" params repoStates = ("TBC", repoStates)
-executeCommand "cat" params repoStates = ("TBC", repoStates)
+-- executeCommand "cat" params repoStates = ("Locating file..." ++ (cat (getRepoState (params !! 0) repoStates) (params !! 1) (params !! 2)), repoStates)
 executeCommand "checkout" params repoStates = ("TBC", repoStates)
 executeCommand "commit" params repoStates = ("Commit successful.", (applyToRepoState (commit (params !! 1)) (params !! 0) repoStates))
 executeCommand "log" params repoStates = ((log_ (getRepoState (params !! 0) repoStates) (tail params)), repoStates)
@@ -182,7 +187,7 @@ heads :: RepositoryState -> [String] -> String
 heads repoState flags =
    let (repo, hd, _) = repoState
    in if null flags
-      then concatMap V.printRevision (getHeads repo)
+      then intercalate "\n" (map V.printRevision (getHeads repo))
       else V.printRevision hd
 
 getHeads :: Repository -> [Revision]
@@ -209,7 +214,17 @@ compNodes (_, x) (_, y) (_, t) =
 -- TBC
 
 -- cat :: RepositoryState -> RevisionID -> FileName -> String
--- TBC
+-- cat repo revId fn = 
+--    case getFile repo revID fn of
+--       Nothing -> "The file was not found. Make sure file name is correct."
+--       Just f -> M.getStringContents f
+
+-- getFile :: RepositoryState -> RevisionID -> FileName -> Maybe(File)
+-- getFile (repo , _, _) revID fn =
+--    let (repId, revisions, manifest, files) = repo
+--    in 
+--    -- (revId, nodeId) = getRevision revId repo 
+--    -- file = getFileLog filename [filelog]
 
 -- checkout :: 
 -- TBC
